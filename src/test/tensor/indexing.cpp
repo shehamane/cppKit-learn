2e2dd0d999f68c9b5259f468bd0b4cda3f43b222
shehamane <shehamane@mail.ru>
2023-06-19 18:25:14 +0300

    New tests

--- src/test/tensor/indexing.cpp
+++ src/test/tensor/indexing.cpp
@@ -1,30 +1,94 @@
-#include "googletest/googletest/include/gtest/gtest.h"
-
 #include "Tensor.h"
 
 class TensorTest : public ::testing::Test {
 protected:
-    Tensor<double> *tensor;
-
+    Tensor<double> *tensor3d{};
+    Tensor<int> *tensor1d{};
 
     void SetUp() override {
-        tensor = new Tensor<double>({5, 6, 7});
-        for (auto i = tensor->begin(); i != tensor->end(); ++i) {
+        tensor3d = new Tensor<double>({5, 6, 7});
+        for (auto i = tensor3d->begin(); i != tensor3d->end(); ++i) {
             *i = i.index().toFlat();
+        }
+
+        tensor1d = new Tensor<int>({100});
+        for (int i = 0; i < 100; ++i) {
+            (*tensor1d)[{i}] = i;
         }
     }
 
     void TearDown() override {
-        delete tensor;
+        delete tensor3d;
+        delete tensor1d;
     };
 };
 
 TEST_F(TensorTest, GetByIndices_FirstElem_Returns0) {
-    auto val = (*tensor)[{0, 0, 0}];
-    ASSERT_EQ(val, 0);
+    ASSERT_EQ(tensor3d->operator[]({0, 0, 0}), 0);
+    ASSERT_EQ(tensor1d->operator[](0), 0);
 }
 
-TEST_F(TensorTest, GetByIndicies_LastElem_Returns209){
-    auto val = (*tensor)[{4, 5, 6}];
-    ASSERT_EQ(val, 209);
+TEST_F(TensorTest, GetByIndicies_LastElem_Returns209) {
+    ASSERT_EQ(tensor3d->operator[]({4, 5, 6}), 209);
+    ASSERT_EQ(tensor1d->operator[](99), 99);
+}
+
+TEST_F(TensorTest, SetByIndicies_Elem_Correct) {
+    double testValue3d = 999;
+    int testValue1d = 127;
+
+    (*tensor3d)[{4, 5, 6}] = testValue3d;
+    (*tensor1d)[{10}] = testValue1d;
+
+    ASSERT_EQ(tensor3d->operator[]({4, 5, 6}), testValue3d);
+    ASSERT_EQ(tensor1d->operator[](10), testValue1d);
+}
+
+TEST_F(TensorTest, GetAndSet_ByIndex_ChangesApplied) {
+    Index index(tensor3d->shape(), {2, 3, 4});
+
+    auto val = tensor3d->at(index);
+    tensor3d->at(index) = 933;
+
+    ASSERT_EQ(val, index.toFlat());
+    ASSERT_EQ(tensor3d->at(index), 933);
+    ASSERT_EQ(tensor3d->operator[]({2, 3, 4}), 933);
+    ASSERT_EQ((int) (*tensor3d)[2][3][4], 933);
+}
+
+TEST_F(TensorTest, IterateOverAll_WithForeach_CorrectOrder) {
+    int i = 0;
+    for (auto val: *tensor1d) {
+        ASSERT_EQ(val, i++);
+    }
+    i = 0;
+    for (auto val: *tensor3d) {
+        ASSERT_EQ(val, i++);
+    }
+}
+
+TEST_F(TensorTest, IterateOverAll_DoSomeChanges_ChangesApplied) {
+    auto tensor1dIt = tensor1d->begin();
+    while (tensor1dIt != tensor1d->end()){
+        *tensor1dIt += 1;
+        ++tensor1dIt;
+    }
+
+    auto tensor3dIt = tensor3d->begin();
+    while (tensor3dIt != tensor3d->end()){
+        *tensor3dIt *= 2;
+        ++tensor3dIt;
+    }
+
+    int i = 0;
+    for (auto val: *tensor1d) {
+        ASSERT_EQ(val, i+1);
+        ++i;
+    }
+
+    i = 0;
+    for (auto val: *tensor3d) {
+        ASSERT_EQ(val, i*2);
+        ++i;
+    }
 }
--- src/test/tensor/initialization.cpp
+++ src/test/tensor/initialization.cpp
@@ -36,17 +36,35 @@ TEST(TensorInitTest, Init_WithZeroShape_ThrowException) {
 
 TEST(TensorInitTest, Init_FromView_EqualData) {
     Tensor<double> tensor1({5, 5, 5});
-    for (Index i = Index::begin(tensor1.shape()); i != Index::end(tensor1.shape()); i.next()){
-        tensor1.at(i) = i.toFlat();
+    for (Index i = Index::begin(tensor1.shape()); i != Index::end(tensor1.shape()); i.next()) {
+        tensor1.at(i) = (double) i.toFlat();
     }
     std::vector<size_t> view_shape = {3, 3, 3};
-    View<double> view(tensor1, {{0, 3, 1}, {0, 3, 1}, {0, 3, 1}});
+    View<double> view(tensor1, {{0, 3, 1},
+                                {0, 3, 1},
+                                {0, 3, 1}});
     Tensor<double> tensor2 = view;
 
     EXPECT_EQ(tensor2.size(), 27);
     EXPECT_EQ(tensor2.dims(), 3);
     EXPECT_EQ(tensor2.shape(), view_shape);
-    for (Index i = Index::begin(tensor2.shape()); i != Index::end(tensor2.shape()); i.next()){
+    for (Index i = Index::begin(tensor2.shape()); i != Index::end(tensor2.shape()); i.next()) {
         EXPECT_EQ(tensor2.at(i), view.at(i));
+    }
+}
+
+TEST(TensorInitTest, Init_FromConstView_EqualData) {
+    Tensor<double> tensor1({5, 5, 5});
+    for (Index i = Index::begin(tensor1.shape()); i != Index::end(tensor1.shape()); i.next()) {
+        tensor1.at(i) = (double) i.toFlat();
+    }
+    std::vector<size_t> viewShape = {5, 5};
+    Tensor<double> tensor2 = tensor1[3];
+
+    EXPECT_EQ(tensor2.size(), 25);
+    EXPECT_EQ(tensor2.dims(), 2);
+    EXPECT_EQ(tensor2.shape(), viewShape);
+    for (Index i = Index::begin(tensor2.shape()); i != Index::end(tensor2.shape()); i.next()) {
+        EXPECT_EQ(tensor2.at(i), tensor1[3].at(i));
     }
 }
