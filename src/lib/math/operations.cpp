c538626d4f1df4610334d17e05b2debb6f8347c6
shehamane <shehamane@mail.ru>
2023-12-21 03:00:18 +0300

    libs targeting modifiers

--- src/lib/math/operations.cpp
+++ src/lib/math/operations.cpp
@@ -4,6 +4,8 @@
 #include "broadcasting.cpp"
 #include "operators.cpp"
 
+#define Shape std::vector<size_t>
+
 
 template<typename T>
 Tensor<T> add(const Tensor<T> &t1, const Tensor<T> &t2) {
@@ -34,3 +36,43 @@ template<typename T>
 Tensor<T> power(const Tensor<T> &t1, const Tensor<T> &t2) {
     return broadcastOperator(t1, t2, powOp);
 };
+
+
+std::vector<size_t> permute(std::vector<size_t> a, std::vector<size_t> permutation) {
+    std::vector<size_t> res(a.size());
+
+    for (int i = 0; i < a.size(); ++i) {
+        res[i] = a[permutation[i]];
+    }
+    return res;
+}
+
+
+template<typename T>
+Tensor<T> transposeOperation(const Tensor<T> &t, std::vector<size_t> permutation = {}) {
+    if (t.dims() == 1) {
+        return Tensor<T>(t);
+    } else {
+        if (permutation.size() == 0) {
+            permutation.resize(t.shape().size());
+            for (int i = 0; i < t.dims() - 1; ++i) {
+                permutation[i] = i + 1;
+            }
+            permutation[t.dims() - 1] = t.dims() - 2;
+        } else if (permutation.size() != t.dims()) {
+            throw std::invalid_argument("Permutation cannot be applied to Tensor shape");
+        }
+
+        Tensor<T> res(permute(t.shape(), permutation));
+
+        Index tIndex = Index::begin(t.shape());
+        Index transposedIndex = Index::begin(t.shape());
+
+        while (!tIndex.isOut()) {
+            transposedIndex = Index(t.shape(), permute(tIndex.indices(), permutation));
+            res[transposedIndex] = t.at(tIndex);
+            tIndex.next();
+        }
+        return res;
+    }
+}
